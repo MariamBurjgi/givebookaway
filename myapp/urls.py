0504deaaaa2232ca2247bec898913fd24b10e0fd
programming <MariamBurjgi>
2023-09-28 10:18:04 +0400

    swagger add

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList, BookDetail
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+    path('books/', BookList.as_view()),
+    
     path('books/<int:pk>/', BookDetail.as_view(), name='book-detail'),
 ]
